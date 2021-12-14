c823f964244bb79efea5e52b6cf95dfef9ce057f
BTS2 <b.tsibere@yahoo.com>
2021-12-13 22:41:09 -0500

    first commit

---
+++ number.py
@@ -0,0 +1,8 @@
+a = input("Enter a = ")
+
+b = int(a) + int(1)
+c = int(a) - int(1)
+
+print()
+print("The next number for the number " + a + " is", b)
+print("The previous number for the number " + a + " is", c)
